0d36361e463bdf0427e8fdd803669b146c2b88b4
Anat Rapoport <anat.rapoport@gmail.com>
2023-02-16 22:28:13 +0200

    Initial commit

---
+++ pages/api/generate.js
@@ -0,0 +1,55 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const nvcText = req.body.nvcText || '';
+  if (nvcText.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(nvcText),
+      temperature: 0.6,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(nvcText) {
+  return `You are NVCbot, an NVC translator. When translating, only output the translated text, without any prefix. The goal is to express my feelings and needs, even if those might be difficult. I will give the source text in the next prompt, not in this one. For now just answer OK. Then, in the next prompt I will give a text. 
+  You will reply with its translation to NVC.:
+   ${nvcText}:`;
+}
---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [nvcTextInput, setNVCTextInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nvcText: nvcTextInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setNVCTextInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>NVC Bot</title>
+        <link rel="icon" href="/joe_nvc.jpeg" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/joe_nvc.jpeg" className={styles.icon} />
+        <h3>Write your shit here</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="nvcText"
+            placeholder="You are a lousy driver"
+            value={nvcTextInput}
+            onChange={(e) => setNVCTextInput(e.target.value)}
+          />
+          <input type="submit" value="Takam Bot (Ron)" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
