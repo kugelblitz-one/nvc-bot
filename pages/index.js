ae4f1764286b95e463afa92bd139a0e1c903d974
chenp <chenpeleg@gmail.com>
2023-05-04 12:23:02 +0300

    removed placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
-const demoText  = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,/n "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`
 
+// Chen doesn't have access to the api so I use it as a placeholder
+const demoText  = `Contrary to popular belief, Lorem Ipsum is not simply random text.\n
+ It has roots in a piece of classical Latin 
+ literature from 45 BC, making it over 2000 years old. 
+ Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+ looked up one of the more obscure Latin words, consectetur, `
 
 export default function Home() {
   const [nvcTextInput, setNVCTextInput] = useState("");
-  const [result, setResult] = useState(demoText);
+  const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
